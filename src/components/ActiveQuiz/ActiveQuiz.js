936bbc6c5d8c4f522e47fa84d52a0ac55a9ad90f
Korolev Pavel <korolev.vrn@gmail.com>
2020-09-12 20:51:49 +0300

    add ActiveQuiz

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from './ActiveQuiz.module.css'
+
+const ActiveQuiz = props => (
+    <div className={classes.ActiveQuiz}>
+        <p className={classes.Question}>
+            <span>
+                <strong>1.</strong>&nbsp;
+                Who are you?
+            </span>
+            <small>4 из 12</small>
+        </p>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+)
+
+export default ActiveQuiz
