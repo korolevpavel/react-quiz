5a47237e488959df8c1e6a75d527564b20e59dd5
Korolev Pavel <korolev.vrn@gmail.com>
2020-10-14 12:37:39 +0300

    Updated navigation when quiz ends

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from './FinishedQuiz.module.css'
 import Button from "../UI/Button/Button";
+import {Link} from "react-router-dom";
 
 const FinishedQuiz = props => {
 
@@ -35,6 +36,9 @@ const FinishedQuiz = props => {
 
             <div>
                 <Button onClick={props.onRetry} type="primary">Повторить</Button>
+                <Link to='/'>
+                    <Button type="success">Список тестов</Button>
+                </Link>
             </div>
         </div>
     )
