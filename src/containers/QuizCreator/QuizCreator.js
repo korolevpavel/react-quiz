30626a60b7c4e3bddf88d6d1cebc01fdf81cf362
Korolev Pavel <korolev.vrn@gmail.com>
2020-10-20 10:55:00 +0300

    Update quiz creator form

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,8 +1,47 @@
 import React, {Component} from 'react';
 import classes from './Quiz.module.css'
 import Button from "../../components/UI/Button/Button";
+import {createControl} from '../../form/formFramework'
+import Input from "../../components/UI/Input/Input";
+import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
+
+function createFormControls() {
+    return {
+            question: createControl(
+                {
+                    label: 'Введите вопрос',
+                    errorMessage: 'Вопрос не может быть пустым'
+                },
+                {
+                    required: true
+                }),
+            option1: createOptionControl(1),
+            option2: createOptionControl(2),
+            option3: createOptionControl(3),
+            option4: createOptionControl(4),
+        }
+}
+
+function createOptionControl(number) {
+    return createControl(
+        {
+                    label: `Вариант ${number }`,
+                    errorMessage: 'Значение не может быть пустым',
+                    id: number
+                },
+                {
+                    required: true
+                }
+    )
+}
 
 class QuizCreator extends Component {
+
+    state = {
+        quiz: [],
+        formControls: createFormControls()
+
+    }
 
     submitHandler = event => {
         event.preventDefault()
@@ -16,37 +55,60 @@ class QuizCreator extends Component {
 
     }
 
+    changeHandler = (value, controlName) => {
+
+    }
+
+    renderControls() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+
+            return (
+                <Auxiliary key={controlName+index}>
+                    <Input
+                        label={control.label}
+                        value={control.value}
+                        valid={control.valid}
+                        shouldValidate={!control.validaton}
+                        touched={control.touched}
+                        errorMessage={control.errorMessage}
+                        onChange={event => this.changeHandler(event.target.value, controlName)}
+                    />
+                    {index === 0 ? <hr /> : null}
+                </Auxiliary>
+            )
+        })
+    }
+
     render() {
         return (
             <div className={classes.QuizCreator}>
+                <div>
                 <h1>Создание текста</h1>
 
-                <form onSubmit={this.submitHandler}>
+                    <form onSubmit={this.submitHandler}>
 
-                    <input type={'text'}/>
-                    <hr/>
-                    <input type={'text'}/>
-                    <input type={'text'}/>
-                    <input type={'text'}/>
+                        {this.renderControls()}
 
 
-                    <select></select>
+                        <select></select>
 
-                    <Button
-                        type={'primary'}
-                        onClick={this.addQuestionHandler}
-                    >
-                        Добавить вопрос
-                    </Button>
+                        <Button
+                            type={'primary'}
+                            onClick={this.addQuestionHandler}
+                        >
+                            Добавить вопрос
+                        </Button>
 
-                    <Button
-                        type={'success'}
-                        onClick={this.createQuizHandler}
-                    >
-                        Создать тест
-                    </Button>
+                        <Button
+                            type={'success'}
+                            onClick={this.createQuizHandler}
+                        >
+                            Создать тест
+                        </Button>
 
-                </form>
+                    </form>
+                </div>
             </div>
         );
     }
