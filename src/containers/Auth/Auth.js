fc60220f136c31f5e0c924c2b5812429271f95df
Korolev Pavel <korolev.vrn@gmail.com>
2020-10-22 10:20:33 +0300

    Added sing up & sing in from rest auth firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import classes from './Auth.module.css'
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
+import axios from 'axios'
 
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -40,11 +41,37 @@ class Auth extends Component {
         }
     }
 
-    loginHandler = () => {
+    loginHandler = async () => {
+
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        }
+
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA4vahXAGk0omMHydx7357nAXrOwPs78Sg', authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
 
     }
 
-    registerHandler = () => {
+    registerHandler = async () => {
+
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        }
+
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA4vahXAGk0omMHydx7357nAXrOwPs78Sg', authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
 
     }
     submitHandler = event => {
