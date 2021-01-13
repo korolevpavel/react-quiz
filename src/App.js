ea9aa18c363ff10cdfdecff06d63ca13e8e8ed22
Korolev Pavel <korolev.vrn@gmail.com>
2021-01-13 12:08:47 +0300

    Add AutoLogin

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ import QuizCreator from "./containers/QuizCreator/QuizCreator";
 import QuizList from "./containers/QuizList/QuizList";
 import {connect} from "react-redux";
 import Logout from "./components/Logout/Logout";
+import {autoLogin} from "./store/actions/auth";
 
 class App extends Component {
+
+    componentDidMount() {
+        this.props.autoLogin()
+    }
 
     render() {
 
@@ -16,7 +21,7 @@ class App extends Component {
             <Switch>
                 <Route path="/auth" component={Auth}/>
                 <Route path="/quiz/:id" component={Quiz}/>
-                <Route path="/" component={QuizList}/>
+                <Route path="/" exact component={QuizList}/>
                 <Redirect to={"/"} />
             </Switch>
         )
@@ -26,8 +31,8 @@ class App extends Component {
                 <Switch>
                     <Route path="/quiz-creator" component={QuizCreator}/>
                     <Route path="/quiz/:id" component={Quiz}/>
-                    <Route path="/" component={QuizList}/>
                     <Route path="/logout" component={Logout}/>
+                    <Route path="/" exact component={QuizList}/>
                     <Redirect to={"/"} />
                 </Switch>
             )
@@ -47,4 +52,10 @@ function mapStateToProps(state) {
     }
 }
 
-export default withRouter(connect(mapStateToProps)(App));
+function mapDispatchToProps(dispatch) {
+    return {
+        autoLogin: () => dispatch(autoLogin())
+    }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
